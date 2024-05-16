69bc16f3a140a633b1e755b1d5bfc493fa8a32e3
Hadi-Kabbani <hadikabbani76917223@gmail.com>
2024-05-17 01:31:17 +0300

    update button change

--- src/transaction-project/Transaction.js
+++ src/transaction-project/Transaction.js
@@ -1,4 +1,4 @@
-function Trasaction({ items, updateTransaction }) {
+function Trasaction({ items, updateTransaction, DeleteTransaction }) {
     function updateTransactionList(e) {
         let updatedTransaction = {
             amount: e.children[0].textNodes,
@@ -10,14 +10,17 @@ function Trasaction({ items, updateTransaction }) {
     function updateTransactionList(e) {
         let index = e.classList[0];
         updateTransaction(index);
-
+    }
+    function deleteTransaction(e) {
+        let index = e.classList[0];
+        DeleteTransaction(index);
     }
     return items.map((ele, index) => (
         <li key={index} className={`${ele.className} w-100 d-flex align-items-center justify-content-evenly ${ele.amount < 0 ? "danger text-bg-danger" : "success text-bg-success"} p-1 mt-2 mb-2`}>
             <span className="text-light fs-5 fw-bold">{ele.description}</span>
             <span className="-text-light fs-6">{ele.amount}</span>
             <button className={`${ele.amount < 0 ? "btn btn-danger p-2" : "btn btn-success p-2"} fs-6`} onClick={(e) => updateTransactionList(e.target.parentNode)}>update</button>
-            < button className={`danger-btn`} >x</button>
+            < button className={`danger-btn`} onClick={(e) => deleteTransaction(e.target.parentNode)}>x</button>
         </li >
     ));
 }
--- src/transaction-project/TransactionList.js
+++ src/transaction-project/TransactionList.js
@@ -1,10 +1,13 @@
 import Trasaction from './Transaction';
-function TransactionList({ items, update }) {
+function TransactionList({ items, update, Delete }) {
     function updateTransactionList(e) {
         update(e);
     }
+    function DeleteTransactionList(e) {
+        Delete(e);
+    }
     return <ul className='list-unstyled transaction-list'>
-        <Trasaction items={items} updateTransaction={updateTransactionList} />
+        <Trasaction items={items} updateTransaction={updateTransactionList} DeleteTransaction={DeleteTransactionList} />
     </ul>
 }
 export default TransactionList;
